99a8a484a3f383167f0a068e5365e4e0770207d1
Yunjaejo <yunjae.dev@gmail.com>
2022-10-26 14:26:34 +0900

    feat: SuccessInterceptor

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -9,20 +9,24 @@ import {
   Post,
   Put,
   UseFilters,
+  UseInterceptors,
 } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { HttpExceptionFilter } from '../common/exceptions/http-exception.filter';
 import { PositiveIntPipe } from '../pipes/positiveint.pipe';
+import { SuccessInterceptor } from '../common/interceptors/success.interceptor';
 
 @Controller('cats')
+@UseInterceptors(SuccessInterceptor)
 @UseFilters(HttpExceptionFilter)
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Get('/')
   getAllCats() {
-    throw new HttpException('api broken', 401);
-    return 'all cats';
+    // throw new HttpException('api broken', 401);
+    console.log('hello controller');
+    return { cats: 'get all cat api' };
   }
 
   @Get('/:id')
