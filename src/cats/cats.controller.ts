5abb530b4443c233fa59b6b5cf326f6b9764839c
Yunjaejo <yunjae.dev@gmail.com>
2022-10-28 14:33:34 +0900

    feat: swagger 작성, dto schema 재사용

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -8,14 +8,17 @@ import {
   ParseIntPipe,
   Patch,
   Post,
-  Put, Req,
+  Put,
+  Req,
   UseFilters,
-  UseInterceptors
+  UseInterceptors,
 } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { HttpExceptionFilter } from '../common/exceptions/http-exception.filter';
 import { SuccessInterceptor } from '../common/interceptors/success.interceptor';
 import { CatsRequestDto } from './dto/cats.request.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ReadOnlyCatDto } from './dto/cat.dto';
 
 @Controller('cats')
 @UseInterceptors(SuccessInterceptor)
@@ -23,26 +26,39 @@ import { CatsRequestDto } from './dto/cats.request.dto';
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
+  @ApiOperation({ summary: '최신 고양이 불러오기' })
   @Get('/')
   getCurrentCat() {
     return 'current cat';
   }
 
+  @ApiOperation({ summary: '회원가입' })
+  @ApiResponse({
+    status: 500,
+    description: 'Server Error...',
+  })
+  @ApiResponse({
+    status: 200,
+    description: '성공!',
+    type: ReadOnlyCatDto,
+  })
   @Post('/')
   async signUp(@Body() body: CatsRequestDto) {
     return await this.catsService.signUp(body);
   }
-
+  @ApiOperation({ summary: '로그인' })
   @Post('login')
   logIn() {
     return 'login';
   }
 
+  @ApiOperation({ summary: '로그아웃' })
   @Post('logout')
   logOut() {
     return 'logout';
   }
 
+  @ApiOperation({ summary: '고양이 이미지 업로드'})
   @Post('upload/cats')
   uploadCatImg() {
     return 'uploadImg';
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,28 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('cat!!')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const port = process.env.PORT;
   await app.listen(port);
 }
+
 bootstrap();
