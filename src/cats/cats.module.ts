204c41ae3e524bdcc51bdd5de2d15443cfb66bce
Yunjaejo <yunjae.dev@gmail.com>
2022-10-28 16:45:14 +0900

    feat: CatsRepository 분리, DI

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -3,12 +3,13 @@ import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Cat, CatsSchema } from './cats.schema';
+import { CatsRepository } from './cats.repository';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Cat.name, schema: CatsSchema }]),
   ],
   controllers: [CatsController],
-  providers: [CatsService],
+  providers: [CatsService, CatsRepository],
 })
 export class CatsModule {}
---
+++ src/cats/cats.repository.ts
@@ -0,0 +1,18 @@
+import { HttpException, Injectable } from '@nestjs/common';
+import { Cat } from './cats.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CatsRequestDto } from './dto/cats.request.dto';
+
+@Injectable()
+export class CatsRepository {
+  constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
+
+  async existsByEmail(email: string): Promise<object> {
+    return this.catModel.exists({ email });
+  }
+
+  async create(cat: CatsRequestDto): Promise<Cat> {
+    return await this.catModel.create(cat);
+  }
+}
